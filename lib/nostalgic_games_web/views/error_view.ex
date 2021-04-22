44a6c2027bac8afe69ee686fc0a880c9c5077b5c
Rafael Corazzi <rafael.corazzi@hotmail.com>
2021-04-22 09:22:16 -0300

    fallback erros

--- lib/nostalgic_games_web/views/error_view.ex
+++ lib/nostalgic_games_web/views/error_view.ex
@@ -13,4 +13,7 @@ defmodule NostalgicGamesWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+  def render("400.json", %{result: result}) do
+
+  end
 end
