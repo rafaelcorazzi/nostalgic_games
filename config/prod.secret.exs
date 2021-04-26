2380dcdb036176598ae0eca4d28165423e97480f
Rafael Corazzi <rafael.corazzi@hotmail.com>
2021-04-25 21:14:38 -0300

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,28 +4,17 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+database_url = "postgres://iyuqnepkjlbnwg:8a53d27b994b502686838c5a15925e00d36bedf63a82c46d48e89137822fcfe3@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d5dhklu17gstgm"
 
 config :nostalgic_games, NostalgicGames.Repo,
   # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
 
 config :nostalgic_games, NostalgicGamesWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: 80 || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
